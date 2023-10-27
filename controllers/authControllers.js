07dda6985f2eaf5545f7ad3050aac6e062eaa972
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-10-27 14:07:01 +0700

    auth controllers

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -26,18 +26,74 @@ module.exports = {
     },
 
     login: async (req, res) => {
+        const findUser = await prisma.mahasiswa.findFirst({
+            where: {
+                nim: req.body.nim
+            }
+        })
 
+        if(!findUser){
+            return res.status(404).json({
+                error: 'NIM tidak terdaftar'
+            });
+        }
+
+        if(bcrypt.compareSync(req.body.password, findUser.password)) {
+            const token = jwt.sign({id: findUser.id}, 'secret_key', { expiresIn : '6h'})
+
+            return res.status(200).json({
+                data: {
+                    token
+                }
+            })
+        }
+
+        return res.status(403).json({
+            error: 'Invalid Credentials'
+        });
     },
 
     getProfile: async (req, res) => {
+        const user = await prisma.mahasiswa.findUnique({
+            where: {
+                id: res.mahasiswa.id
+            }
+        })
 
+        return res.status(200).json({
+            data: user
+        })
     },
 
-    getUser: async (req, res) => {
-
+    getMahasiswa: async (req, res) => {
+        const user = await prisma.mahasiswa.findMany();
+        
+        return res.json({
+            data: user
+        });
     },
 
-    getUserId: async (req, res) => {
-
+    getMahasiswaId: async (req, res) => {
+        const mahasiswaId = parseInt(req.params.mahasiswaId);
+    
+        const user = await prisma.mahasiswa.findUnique({
+            where: {
+                id: mahasiswaId
+            },
+            include: {
+                images: true,
+                videos: true
+            }
+        });
+    
+        if (!user) {
+            return res.status(404).json({
+                error: 'Data mahasiswa tidak ditemukan'
+            });
+        }
+    
+        return res.status(200).json({
+            data: user
+        });
     }
-}
+}    
