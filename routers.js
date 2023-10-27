ca3dfe66ccb18c272d2cf1ea7fe125c50d998671
21081010047AdeliaPutriAdyani <21081010047@student.upnjatim.ac.id>
2023-10-27 14:06:23 +0700

    router fix

--- routers.js
+++ routers.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const multer = require('./middlewares/multer')
 const authController = require('./controllers/authControllers')
+const checkToken = require('./middlewares/checkToken')
 const imageController = require('./controllers/imageControllers')
 const videoController = require('./controllers/videoControllers')
 
@@ -11,7 +12,18 @@ router.get('/', (req, res) => {
     })
 })
 
-router.post('/register', authController.register),
+router.post('/register', authController.register)
+router.post('/login', authController.login)
+router.get('/authenticate', checkToken, authController.getProfile)
+router.get('/mahasiswa', authController.getMahasiswa)
+router.get('/mahasiswa/:mahasiswaId', authController.getMahasiswaId)
+
 router.post('/upImage', multer.image.single('image'), imageController.upImage),
+router.get('/image', imageController.getImage)
+router.get('/image/:imageId', imageController.getImageId)
+
 router.post('/upVideo', multer.video.single('video'), videoController.upVideo)
+router.get('/video', videoController.getVideo)
+router.get('/video/:videoId', videoController.getVideoId)
+
 module.exports = router
